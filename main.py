8bd124e6c4c7ef56a0a6590b0939f3c683b3abe6
Stephen <slpotgieter1@gmail.com>
2022-06-21 12:07:57 +0100

    commit 4 - add timer & count functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,7 +14,22 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(300)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+
+
+    canvas.itemconfig(timer_text, text=f"{count_min}: {count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,13 +37,16 @@ window = Tk()
 window.title("Pomodoro study tool")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
 #create canvas and saves the image directory
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 
 #create the image and text on the canvas
 canvas.create_image(100,112, image=tomato_img)
-canvas.create_text(103,130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(103,130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+
 
 #labels
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=("Calibri", 24,"bold"))
@@ -36,7 +55,7 @@ check_mark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, "
 check_mark_label.grid(column=2, row=3)
 
 #buttons
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=1, row=3)
 reset_button = Button(text="Reset")
 reset_button.grid(column=3, row=3)
